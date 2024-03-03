add6f46a4eae5539ad94943e19e57f1c134e51a2
BaronLegba <142911344+BaronLegba@users.noreply.github.com>
2024-03-03 12:34:44 +0400

    Add files via upload

---
+++ seminar4/task5/A.java
@@ -0,0 +1,10 @@
+package seminar4.task5;
+
+public class A {
+}
+
+class B extends A { /* ... */ }
+
+class C extends B { /* ... */ }
+
+class D extends C { /* ... */ } // D is not a subclass of A
---
+++ seminar4/task5/Gen.java
@@ -0,0 +1,9 @@
+package seminar4.task5;
+
+class Gen<T> {
+    T obj;
+
+    Gen(T o) {
+        obj = o;
+    }
+}
